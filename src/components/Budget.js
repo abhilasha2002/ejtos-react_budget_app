df4f859c63344698c04e87ece6ff103867ef6117
abhilasha2002 <145214688+abhilasha2002@users.noreply.github.com>
2023-09-17 00:46:39 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,47 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+const Budget = ({ remaining, selectedCurrency }) => {
+  const { budget, setBudget } = useContext(AppContext);
+  const [editableBudget, setEditableBudget] = useState(budget);
+
+  const { expenses } = useContext(AppContext);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
+  const handleSave = () => {
+    if (editableBudget > 20000) {
+      alert("The value cannot exceed than 20000");
+      setEditableBudget(budget); // Reset to original budget value
+      return;
+    } else if (editableBudget < totalExpenses) {
+      alert("The value cannot be less than spending");
+      setEditableBudget(budget); // Reset to original budget value
+      return;
     }
-    return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
+    setBudget(editableBudget);
+  };
+
+  return (
+    <div className='alert alert-secondary'>
+      <label htmlFor='budgetInput'>Budget:</label>
+      <div className='input-group'>
+        <div className='input-group-prepend'>
+          <span className='input-group-text'>{ selectedCurrency }</span>
+        </div>
+        <input
+          required='required'
+          type='number'
+          id='budgetInput'
+          value={editableBudget}
+          style={{ width: '8rem' }}
+          onChange={(event) => setEditableBudget(parseInt(event.target.value))}
+        />
+        <button onClick={handleSave}>Save</button>
+      </div>
+    </div>
+  );
 };
+
 export default Budget;
